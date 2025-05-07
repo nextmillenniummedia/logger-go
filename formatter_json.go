bdc7b7a5e3c02be7c5284b201ad7efdfa1f007bc
a.popov <wt.auf.borsig@gmail.com>
2025-05-07 13:31:10 +0300

    PB-3537 added null value instead error

--- formatter_json.go
+++ formatter_json.go
@@ -13,13 +13,25 @@ type formatterJson struct {
 }
 
 func (f *formatterJson) Format(params FormatParams) (result []byte, err error) {
+	checkForEmptyRaw(params)
 	formatted, err := json.Marshal(params)
 	if err != nil {
 		return nil, err
 	}
-	return []byte(fmt.Sprintf("%s\n", string(formatted))), nil
+	return fmt.Appendf(nil, "%s\n", string(formatted)), nil
 }
 
 func (f *formatterJson) Clone() IFormatter {
 	return &formatterJson{}
+}
+
+func checkForEmptyRaw(params FormatParams) {
+	for key, value := range params {
+		switch valyeTyped := value.(type) {
+		case json.RawMessage:
+			if len(valyeTyped) == 0 {
+				params[key] = json.RawMessage(`null`)
+			}
+		}
+	}
 }
